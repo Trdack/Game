50f119e970a0610d781ed2339f6607737ebf394e
Contsit <1303256753>
2025-01-07 17:47:19 +0800

    贪吃蛇 => feat: <game_snake.js> 游戏开始界面

--- js/modules/game_snake.js
+++ js/modules/game_snake.js
@@ -18,28 +18,74 @@ class SnakeGame {
         score: 0,           // 成绩
         snakeElem: [],      // 蛇元素
     };
-    #UI = {
-
+    #style = {
+        center: {
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+        },
     };
-    constructor() {
-        console.log('SnakeGame');
+    #UI = {};
+    constructor(gameWindow) {
+        this.#UI.gameWindow = gameWindow;
+        console.log(this.#UI.gameWindow)
         this.#init();
         this.#main();
     }
 
+    // Finish: 根据样式新建div
+    #fndiv(style) {
+        let div = document.createElement("div");
+        for (let key in style) {
+            div.style[key] = style[key];
+        }
+        return div;
+    }
+
     // TODO: 初始化
     #init() {
+        // 开始界面
+        this.#startInterface();
+        // 游戏开始菜单
         // 初始数据
+        this.#initData();
         // 初始化界面
     }
 
+    // TODO: 初始化数据
     #initData() {
-        
+        console.log('初始化数据');
+        this.#data.snake = [[1, 2],[2, 2],[3, 2],[4, 2],[5, 2],[6, 2],[7, 2]];
+        console.log(this.#data.snake);
+    }
+
+    // TODO: 开始界面
+    #startInterface() {
+        let style = {
+            ...this.#style.center,
+            width: "900px",
+            height: "500px",
+            border: "1px solid #222",
+            color: "#bad",
+            fontSize: "108px",
+            textAlign: "center",
+            lineHeight: "180px",
+            fontWeight: "900",
+            textShadow: "5px 5px 5px blue",
+            // borderRadius: "180px",
+        };
+        let elem = this.#fndiv(style);
+        this.#UI.gameWindow.appendChild(elem);
+        this.#UI.startInterfaceElem = elem;
     }
 
 
-    #main() {
 
+
+
+    // TODO: 游戏主程序
+    #main() {
     }
 }
 
